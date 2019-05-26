5fee7f236e7124bbdceccae49d00d890f791b696
Jichao Ouyang <oyanglulu@gmail.com>
2019-05-26 17:40:56 +1000

    fmting

--- src/main/scala/us/oyanglul/JuJiu.scala
+++ src/main/scala/us/oyanglul/JuJiu.scala
@@ -1,39 +1,37 @@
 package us.oyanglul.jujiu
 import cats._
 import cats.data.Kleisli
-import cats.effect.{ Async, Sync }
+import cats.effect.{Async, Sync}
 import cats.syntax.traverse._
 import cats.syntax.parallel._
 import scala.compat.java8.FutureConverters.{toScala}
-import com.github.benmanes.caffeine.cache.{
-  Cache => CCache,
-  AsyncLoadingCache => CALCache,
-  LoadingCache => CLCache
-}
+import com.github.benmanes.caffeine.cache.{Cache => CCache, AsyncLoadingCache => CALCache, LoadingCache => CLCache}
 import scala.concurrent.ExecutionContext
 import scala.util._
 
-trait CaffeineCache[F[_], K, V] extends Cache[F, CCache[K, V], K, V]{
+trait CaffeineCache[F[_], K, V] extends Cache[F, CCache[K, V], K, V] {
   type Ops[A] = Kleisli[F, CCache[K, V], A]
 
   def put(k: K, v: V)(implicit M: Sync[F]): Ops[Unit] =
-    Kleisli{caffeine =>M.delay(caffeine.put(k, v)) }
+    Kleisli { caffeine =>
+      M.delay(caffeine.put(k, v))
+    }
   def fetch(k: K)(implicit M: Sync[F]): Ops[Option[V]] =
     Kleisli(caffeine => M.delay(Option(caffeine.getIfPresent(k))))
   def clear(k: K)(implicit M: Sync[F]): Ops[Unit] =
     Kleisli(caffeine => M.delay(caffeine.invalidate(k)))
 }
 
-trait CaffeineLoadingCache[F[_], K, V] extends LoadingCache[F, CLCache[K,V], K, V] {
-  override def fetch(k: K)(implicit M: Sync[F]): Kleisli[F, CLCache[K,V], V] =
+trait CaffeineLoadingCache[F[_], K, V] extends LoadingCache[F, CLCache[K, V], K, V] {
+  override def fetch(k: K)(implicit M: Sync[F]): Kleisli[F, CLCache[K, V], V] =
     Kleisli(caffeine => M.delay(caffeine.get(k)))
 }
 
 trait CaffeineAsyncLoadingCache[F[_], K, V] extends AsyncLoadingCache[F, CALCache[K, V], K, V] {
   implicit val executionContext: ExecutionContext
   override def fetch(k: K)(implicit M: Async[F]): Kleisli[F, CALCache[K, V], V] =
-    Kleisli{caffeine =>
-      M.async{ cb =>
+    Kleisli { caffeine =>
+      M.async { cb =>
         val future = toScala(caffeine.get(k))
         future.onComplete {
           case Success(v) => cb(Right(v))
@@ -43,14 +41,13 @@ trait CaffeineAsyncLoadingCache[F[_], K, V] extends AsyncLoadingCache[F, CALCach
     }
 }
 
-
 trait Cache[F[_], S, K, V] {
   def put(k: K, v: V)(implicit M: Sync[F]): Kleisli[F, S, Unit]
   def fetch(k: K)(implicit M: Sync[F]): Kleisli[F, S, Option[V]]
   def fetch(k: K, default: K => F[V])(implicit M: Sync[F]): Kleisli[F, S, V] =
     fetch(k).flatMapF(_ match {
       case Some(v) => M.delay(v)
-      case None => default(k)
+      case None    => default(k)
     })
   def fetchAll[L[_]: Traverse](keys: L[K])(implicit M: Sync[F]) =
     keys.traverse(k => fetch(k))
--- src/test/scala/us/oyanglul/JujiuSpec.scala
+++ src/test/scala/us/oyanglul/JujiuSpec.scala
@@ -1,9 +1,7 @@
 package us.oyanglul.jujiu
-import com.github.benmanes.caffeine.cache.{ AsyncCacheLoader,
-  AsyncLoadingCache => ALC,
-  Caffeine }
+import com.github.benmanes.caffeine.cache.{AsyncCacheLoader, AsyncLoadingCache => ALC, Caffeine}
 import scala.concurrent.ExecutionContext
-import java.util.concurrent.{ CompletableFuture, Executor }
+import java.util.concurrent.{CompletableFuture, Executor}
 import org.specs2.mutable.Specification
 import cats.instances.list._
 import cats.effect._
@@ -21,36 +19,42 @@ class JujiuSpec extends Specification {
     } yield (r1, r2, r3, r4)
     program(
       Caffeine
-      .newBuilder().build()
+        .newBuilder()
+        .build()
     ).unsafeRunSync() must_== ((None, "default", Some("now exist"), None))
   }
 
   "it should able to get and set async loading cache" >> {
     import scala.concurrent.ExecutionContext.Implicits.global
-     object cache extends CaffeineAsyncLoadingCache[IO, String, String] {
-       implicit val executionContext = global
-     }
+    object cache extends CaffeineAsyncLoadingCache[IO, String, String] {
+      implicit val executionContext = global
+    }
     implicit val cs: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
     val program = for {
       r1 <- cache.fetch("1")
       r2 <- cache.fetch("2")
-      r3 <- cache.fetchAll[List, IO.Par](List("1","2","3"))
+      r3 <- cache.fetchAll[List, IO.Par](List("1", "2", "3"))
     } yield (r1, r2, r3)
 
-    val caffeine: ALC[String, String] = Caffeine.newBuilder()
+    val caffeine: ALC[String, String] = Caffeine
+      .newBuilder()
       .executor(new Executor {
-        def execute(r: Runnable) = {
+        def execute(r: Runnable) =
           global.execute(r)
-        }
       })
-       .buildAsync(new AsyncCacheLoader[String, String] {
-        def asyncLoad(key: String, executor: Executor) = {
+      .buildAsync(new AsyncCacheLoader[String, String] {
+        def asyncLoad(key: String, executor: Executor) =
           CompletableFuture.supplyAsync(
             () => "async string" + key,
             executor
           )
-        }
       })
-    program(caffeine).unsafeRunSync() must_== (("async string1", "async string2", List("async string1", "async string2", "async string3")))
+    program(caffeine).unsafeRunSync() must_== (
+      (
+        "async string1",
+        "async string2",
+        List("async string1", "async string2", "async string3")
+      )
+    )
   }
 }
