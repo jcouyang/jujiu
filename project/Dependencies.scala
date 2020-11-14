9e452c665d1bfc65147fdaa33f08250b67d77751
Scala Steward <me@scala-steward.org>
2020-11-14 08:34:40 +0100

    Update caffeine to 2.8.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
 
   lazy val caffeine = Seq(
     "org.scala-lang.modules"        %% "scala-java8-compat" % "0.9.1",
-    "com.github.ben-manes.caffeine" % "caffeine"            % "2.7.0"
+    "com.github.ben-manes.caffeine" % "caffeine"            % "2.8.6"
   )
   lazy val specs2 = Seq(
     "org.specs2"     %% "specs2-core" % "4.8.3"  % "it,test",
