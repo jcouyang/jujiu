6735df060d938d6593569579af432f3c322a8739
Jichao Ouyang <oyanglulu@gmail.com>
2020-11-14 11:25:03 +1100

    scalafmtAll

--- src/test/scala/us/oyanglul/JujiuSpec.scala
+++ src/test/scala/us/oyanglul/JujiuSpec.scala
@@ -15,20 +15,20 @@ import scala.concurrent.duration._
 import com.github.benmanes.caffeine.cache
 import us.oyanglul.jujiu.syntax.CaffeineSyntax
 
-class JujiuSpec extends Specification with org.specs2.mock.Mockito{
+class JujiuSpec extends Specification with org.specs2.mock.Mockito {
   implicit val cs: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
 
   "it should able to get and set cache" >> {
-    object cacheDsl extends CaffeineCache[IO, String, String]   // <-
+    object cacheDsl extends CaffeineCache[IO, String, String] // <-
     val program = for {
-      r1 <- cacheDsl.fetch("not exist yet")                     // <-
+      r1 <- cacheDsl.fetch("not exist yet") // <-
       r2 <- cacheDsl.fetch("not exist yet", _ => IO("default")) // <-
-      _ <- cacheDsl.put("not exist yet", "now exist")           // <- 
+      _ <- cacheDsl.put("not exist yet", "now exist") // <-
       r3 <- cacheDsl.fetch("not exist yet")
       _ <- cacheDsl.clear("not exist yet")
       r4 <- cacheDsl.fetch("not exist yet")
     } yield (r1, r2, r3, r4)
-    program(Caffeine().sync)                                    // <- 
+    program(Caffeine().sync) // <-
       .unsafeRunSync() must_== ((None, "default", Some("now exist"), None))
   }
 
@@ -42,7 +42,11 @@ class JujiuSpec extends Specification with org.specs2.mock.Mockito{
     } yield (r1, r2)
 
     val failCache = mock[cache.AsyncCache[String, String]]
-    failCache.getIfPresent("not exist yet") returns CompletableFuture.supplyAsync(() => IO.raiseError[String](new Exception("cache load error")).unsafeRunSync())
+    failCache
+      .getIfPresent("not exist yet")
+      .returns(
+        CompletableFuture.supplyAsync(() => IO.raiseError[String](new Exception("cache load error")).unsafeRunSync())
+      )
 
     program(
       failCache
@@ -74,57 +78,57 @@ class JujiuSpec extends Specification with org.specs2.mock.Mockito{
     }
 
     "loading cache" >> {
-        val c: LoadingCache[IO, cache.LoadingCache, String, String] = new CaffeineLoadingCache[IO, String, String] {}
-        implicit val cacheProvider: cache.LoadingCache[String, String] = Caffeine().sync(identity)
-        def program =
-          for {
-            _ <- IO(println("something"))
-            r1 <- c.fetchF("1")
-            r2 <- c.fetchAllF(List("2", "3"))
-            r3 <- c.parFetchAllF[List, IO.Par](List("4", "5"))
-          } yield (r1, r2, r3)
-        program.unsafeRunSync() must_== (("1", List("2", "3"), List("4", "5")))
-      }
+      val c: LoadingCache[IO, cache.LoadingCache, String, String] = new CaffeineLoadingCache[IO, String, String] {}
+      implicit val cacheProvider: cache.LoadingCache[String, String] = Caffeine().sync(identity)
+      def program =
+        for {
+          _ <- IO(println("something"))
+          r1 <- c.fetchF("1")
+          r2 <- c.fetchAllF(List("2", "3"))
+          r3 <- c.parFetchAllF[List, IO.Par](List("4", "5"))
+        } yield (r1, r2, r3)
+      program.unsafeRunSync() must_== (("1", List("2", "3"), List("4", "5")))
+    }
+  }
+
+  "it should able to get and set async loading cache" >> {
+    object cache extends CaffeineAsyncLoadingCache[IO, Integer, String] {
+      implicit val executionContext = global // <-- (ref:executionContext)
     }
 
-    "it should able to get and set async loading cache" >> {
-      object cache extends CaffeineAsyncLoadingCache[IO, Integer, String] {
-        implicit val executionContext = global          // <-- (ref:executionContext)
-      }
+    val program = for {
+      r1 <- cache.fetch(1)
+      r2 <- cache.fetch(2)
+      r3 <- cache.fetchAll(List[Integer](1, 2, 3))
+    } yield (r1, r2, r3)
 
-      val program = for {
-        r1 <- cache.fetch(1)
-        r2 <- cache.fetch(2)
-        r3 <- cache.fetchAll(List[Integer](1, 2, 3))
-      } yield (r1, r2, r3)
-
-      val caffeineA:com.github.benmanes.caffeine.cache.AsyncLoadingCache[Integer, String] = Caffeine()
-        .executionContext(global)           // <-- (ref:global)
-        .withExpire(                            // <-- (ref:expire)
-          (_: Integer, _: String) => {
-            1.second
-          },
-          (_: Integer, _: String, currentDuration: FiniteDuration) => currentDuration,
-          (_: Integer, _: String, currentDuration: FiniteDuration) => currentDuration
-        )
-        .async((key: Integer) => IO("async string" + key)) // <-- (ref:async)
-
-      val caffeineB = Caffeine()
-        .withExpireAfterAccess(1.second)
-        .withExpireAfterWrite(2.seconds)
-        .withRefreshAfterWrite(3.seconds)
-        .async((key: Integer) => IO("async string" + key))
-
-      val expected = (
-        "async string1",
-        "async string2",
-        List("async string1", "async string2", "async string3")
+    val caffeineA: com.github.benmanes.caffeine.cache.AsyncLoadingCache[Integer, String] = Caffeine()
+      .executionContext(global) // <-- (ref:global)
+      .withExpire( // <-- (ref:expire)
+        (_: Integer, _: String) => {
+          1.second
+        },
+        (_: Integer, _: String, currentDuration: FiniteDuration) => currentDuration,
+        (_: Integer, _: String, currentDuration: FiniteDuration) => currentDuration
       )
-      program(caffeineA).unsafeRunSync() must_== expected
-      program(caffeineB).unsafeRunSync() must_== expected
-      program(Caffeine().async(_ => IO.raiseError(new Exception("something wrong"))))
-        .unsafeRunSync() must throwA[Exception]
-    }
+      .async((key: Integer) => IO("async string" + key)) // <-- (ref:async)
+
+    val caffeineB = Caffeine()
+      .withExpireAfterAccess(1.second)
+      .withExpireAfterWrite(2.seconds)
+      .withRefreshAfterWrite(3.seconds)
+      .async((key: Integer) => IO("async string" + key))
+
+    val expected = (
+      "async string1",
+      "async string2",
+      List("async string1", "async string2", "async string3")
+    )
+    program(caffeineA).unsafeRunSync() must_== expected
+    program(caffeineB).unsafeRunSync() must_== expected
+    program(Caffeine().async(_ => IO.raiseError(new Exception("something wrong"))))
+      .unsafeRunSync() must throwA[Exception]
+  }
 
   "works with tagless final" >> {
     trait LogDsl[F[_]] {
@@ -133,8 +137,7 @@ class JujiuSpec extends Specification with org.specs2.mock.Mockito{
 
     type ProgramDsl[F[_]] = CaffeineCache[F, String, String] with LogDsl[F]
 
-    def program[F[_]: Async](dsl: ProgramDsl[F])
-    (implicit ev: cache.Cache[String, String]): F[Option[String]] =
+    def program[F[_]: Async](dsl: ProgramDsl[F])(implicit ev: cache.Cache[String, String]): F[Option[String]] =
       for {
         value <- dsl.fetchF("key")
         _ <- dsl.log("something")
@@ -191,20 +194,21 @@ class JujiuSpec extends Specification with org.specs2.mock.Mockito{
   "run on redis" >> {
     import redis.clients.jedis._
 
-    def program[F[_]: Async, S[_, _]](dsl: Cache[F, S, String, String]) = for {
-      r1 <- dsl.fetch("not exist yet")
-      r2 <- dsl.fetch("not exist yet", _ => Async[F].delay("default"))
-      _ <- dsl.put("not exist yet", "now exist")
-      r3 <- dsl.fetch("not exist yet")
-      _ <- dsl.clear("not exist yet")
-      r4 <- dsl.fetch("not exist yet")
-    } yield (r1, r2, r3, r4)
+    def program[F[_]: Async, S[_, _]](dsl: Cache[F, S, String, String]) =
+      for {
+        r1 <- dsl.fetch("not exist yet")
+        r2 <- dsl.fetch("not exist yet", _ => Async[F].delay("default"))
+        _ <- dsl.put("not exist yet", "now exist")
+        r3 <- dsl.fetch("not exist yet")
+        _ <- dsl.clear("not exist yet")
+        r4 <- dsl.fetch("not exist yet")
+      } yield (r1, r2, r3, r4)
 
     type J[A, B] = Jedis
     object dsl extends Cache[IO, J, String, String] {
       def put(k: String, v: String)(implicit M: Async[IO]): Kleisli[IO, Jedis, Unit] =
         Kleisli { redis =>
-          M.delay{
+          M.delay {
             redis.set(k, v)
             ()
           }
@@ -212,14 +216,19 @@ class JujiuSpec extends Specification with org.specs2.mock.Mockito{
       def fetch(k: String)(implicit M: Async[IO]): Kleisli[IO, Jedis, Option[String]] =
         Kleisli(redis => M.delay(Option(redis.get(k))))
       def clear(k: String)(implicit M: Async[IO]): Kleisli[IO, Jedis, Unit] =
-        Kleisli(redis => M.delay{
-          redis.del(k)
-          ()
-        })
+        Kleisli(
+          redis =>
+            M.delay {
+              redis.del(k)
+              ()
+            }
+        )
     }
 
-    program(dsl).run(
-      new Jedis("localhost")
-    ).unsafeRunSync() must_== ((None, "default", Some("now exist"), None))
+    program(dsl)
+      .run(
+        new Jedis("localhost")
+      )
+      .unsafeRunSync() must_== ((None, "default", Some("now exist"), None))
   }.pendingUntilFixed("Redis")
 }
