b3c9045db201acbcfdd98f402413565aa840048e
Jichao Ouyang <oyanglulu@gmail.com>
2021-01-26 16:28:11 +1100

    update docs for scala3

--- src/test/scala-3.0.0-M3/us/oyanglul/JujiuSpec.scala
+++ src/test/scala-3.0.0-M3/us/oyanglul/JujiuSpec.scala
@@ -11,17 +11,17 @@ class JujiuScala3Spec extends Specification:
   given ContextShift[IO] = IO.contextShift(ExecutionContext.global)
   "works with IO" >> {
     "normal cache" >> {
-      val c: Cache[IO, cache.Cache, String, String] = new CaffeineCache[IO, String, String]{}
+      val dsl: Cache[IO, cache.Cache, String, String] = new CaffeineCache[IO, String, String]{}
 
-      def program =
+      def program(using cache.Cache[String, String]) =
         for
           _ <- IO(println("something"))
-          _ <- c.putF("key1", "value1")
-          r1 <- c.fetchF("key1")
-          r2 <- c.fetchF("key2", _ => IO("value2"))
-          r3 <- c.fetchAllF(List("key1", "key2"))
-          r4 <- c.parFetchAllF[List, IO.Par](List("key1", "key2"))
-          _ <- c.clearF("key1")
+          _ <- dsl.putF("key1", "value1")
+          r1 <- dsl.fetchF("key1")
+          r2 <- dsl.fetchF("key2", _ => IO("value2"))
+          r3 <- dsl.fetchAllF(List("key1", "key2"))
+          r4 <- dsl.parFetchAllF[List, IO.Par](List("key1", "key2"))
+          _ <- dsl.clearF("key1")
         yield (r1, r2, r3, r4)
 
       given cache.Cache[String, String] = Caffeine().sync[String, String]
