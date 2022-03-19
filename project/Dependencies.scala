1b052490f067a7da1a0b9ee26382e7ab49da1e11
Scala Steward <me@scala-steward.org>
2022-03-19 21:14:25 +0100

    Update cats-effect to 3.3.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   lazy val cats = Seq(
     "org.typelevel" %% "cats-core"   % "2.1.0",
-    "org.typelevel" %% "cats-effect" % "2.5.4"
+    "org.typelevel" %% "cats-effect" % "3.3.8"
   )
 
   lazy val javaCompat = Seq(
