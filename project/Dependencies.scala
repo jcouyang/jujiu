2c5643f2044628951d68bf778ac6453004dc76a9
Scala Steward <scala_steward@virtuslab.com>
2023-01-23 16:04:49 +0000

    Update specs2-core, specs2-mock to 4.19.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,9 +14,9 @@ object Dependencies {
     "com.github.ben-manes.caffeine" % "caffeine" % "2.9.3"
   )
   lazy val specs2 = Seq(
-    "org.specs2"     %% "specs2-core" % "4.19.0" % "it,test",
+    "org.specs2"     %% "specs2-core" % "4.19.1" % "it,test",
     "org.scalacheck" %% "scalacheck"  % "1.17.0" % "it,test",
-    "org.specs2"     %% "specs2-mock" % "4.19.0" % "it,test"
+    "org.specs2"     %% "specs2-mock" % "4.19.1" % "it,test"
   )
 
   lazy val redis = Seq(
